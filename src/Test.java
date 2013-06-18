572f1899e63dcd67c29349cbe05e7507554ef8a9
Robert Gerdisch <rgerdisch3@gmail.com>
2013-06-17 22:12:39 -0400

    Added javadoc to test class.

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,9 @@
 
 public class Test {
+	/**
+	 * Prints a message to the console!
+	 * @param args ARGUMENTS.
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello GitHub!");
 	}
