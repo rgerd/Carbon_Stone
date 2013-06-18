0da45b160931ee1dd1efb8dd3a7f895003619f3f
rgerd <rgerdisch@gmail.com>
2013-06-17 23:16:08 -0300

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,9 @@
-
 public class Test {
 	/**
 	 * Prints a message to the console!
 	 * @param args ARGUMENTS.
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello GitHub!");
+		System.out.println("Hello Gitub!");
 	}
 }
