f45ca0a542315fa6b668672a45c30222891ca395
Robert Gerdisch <rgerdisch@gmail.com>
2013-06-18 23:07:26 -0400

    OMG COMMIT

--- src/Test.java
+++ src/Test.java
@@ -4,6 +4,6 @@ public class Test {
 	 * @param args lolol
 	 */
 	public static void main(String[] args) { 
-		System.out.println("Hello GitHub!!");
+		System.out.println("Hello uoiGitHub!!");
 	}
 }
