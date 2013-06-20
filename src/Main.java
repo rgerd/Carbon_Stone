3c878afd0f8699c9d15673808671c868aad35af4
Robert Gerdisch <rgerdisch@gmail.com>
2013-06-20 15:11:18 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,6 @@ public class Main {
 	//https://github.com/rgerd/Carbon_Stone/commits/master.atom
 	//^hopefully an RSS Feed, idk
 	
-	//If you see this, we will talk tomorrow. 
-	//I can't find my phone right now and I need to go to sleep 'early' for gym.
-	//(6/9/13 @ 10:39 PM)
 	/**
 	 * STARTS THE GAME
 	 */
