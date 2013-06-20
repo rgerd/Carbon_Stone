74d1a60d05c1dd9cdda351c070a8fe058de35861
Robert Gerdisch <rgerdisch@gmail.com>
2013-06-19 20:41:17 -0400

    Changed some things

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+	/**
+	 * STARTS THE GAME
+	 */
+	public static void main(String[] args) { 
+		Game.start();
+	}
+	
+	private static class Game {
+		/**
+		 * ACTUALLY STARTS THE GAME
+		 */
+		public static void start() {
+			System.out.println("OMG YOU STARTED THE GAME");
+		}
+	}
+}
--- src/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-	/**
-	 * JAVADOC WHOOOO? I broke the commit
-	 * @param args lolol
-	 */
-	public static void main(String[] args) { 
-		System.out.println("Hello ofiewoij!!");
-	}
-}
